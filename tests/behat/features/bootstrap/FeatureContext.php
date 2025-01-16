2e4fdb69deaa42762f8690ab29f14d6b4da5088e
Alex Skrypnyk <alex@drevops.com>
2025-01-16 11:19:59 +1100

    Switched to Drupal coding standards.

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -2,29 +2,24 @@
 
 declare(strict_types=1);
 
-/**
- * @file
- * Feature context Behat testing.
- */
-
 use Behat\Behat\Context\Context;
 use Behat\MinkExtension\Context\MinkContext;
 
 /**
+ * Class FeatureContext.
+ *
  * Defines application features from the specific context.
  */
-class FeatureContext extends MinkContext implements Context
-{
+class FeatureContext extends MinkContext implements Context {
 
   /**
-     * Go to the phpserver test page.
-     *
-     *
-     * @Given /^(?:|I )am on (?:|the )phpserver test page$/
-     * @When /^(?:|I )go to (?:|the )phpserver test page$/
-     */
-    public function goToPhpServerTestPage(): void
-    {
-        $this->getSession()->visit('http://localhost:8888/testpage.html');
-    }
+   * Go to the phpserver test page.
+   *
+   * @Given /^(?:|I )am on (?:|the )phpserver test page$/
+   * @When /^(?:|I )go to (?:|the )phpserver test page$/
+   */
+  public function goToPhpServerTestPage(): void {
+    $this->getSession()->visit('http://localhost:8888/testpage.html');
+  }
+
 }
